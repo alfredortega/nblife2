0d319131c5a9746077066179381a89f5b10015fd
Alfred Ortega <alfredortegaiii@gmail.com>
2019-01-07 09:35:49 -0600

    working thought eudonix and did a composer update

--- laravel/lsapp/resources/views/inc/showcase.blade.php
+++ laravel/lsapp/resources/views/inc/showcase.blade.php
@@ -1,6 +1,6 @@
 <div class="jumbotron text-center">
     <div class="container">
-        <h1>Welcome to our site.</h1>
-        <p class="lead">Welcome to our brand new laravel website!!!</p>
+        <h1>Welcome To Our Site.</h1>
+        <p class="lead">Welcome to our brand new laravel powered website!!! This site uses laravel 5.7</p>
     </div>
 </div>
--- laravel/lsapp/resources/views/layouts/app.blade.php
+++ laravel/lsapp/resources/views/layouts/app.blade.php
@@ -28,7 +28,6 @@
                 @include('inc.showcase')
             @endif
             <div class="row">
-                <div class="col-md-8 col-lg-8">
                     @yield('content')
                 </div>
                 <div class="col-md-4 col-lg-4">
@@ -36,7 +35,9 @@
                 </div>
             </div>
 
-
+            <footer id="footer" class="text-center">
+                &copy 2019
+            </footer>
 
     
         </main>
