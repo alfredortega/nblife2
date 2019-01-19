9fc635e42175c6258ace1a4cf107fc0939fbaae9
Alfred Ortega <alfredortegaiii@gmail.com>
2019-01-19 02:29:39 -0600

    Setup is done including todo model and controller

--- todo.dev/routes/web.php
+++ todo.dev/routes/web.php
@@ -1,3 +1,4 @@
+
 <?php
 
 /*
@@ -11,6 +12,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'TodosController@index');
+
+Route::resource('todo','TodosController');
