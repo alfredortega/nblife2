0f3626c48284a6d92ff137f0bd3d5c76fdd7b7a6
Alfred Ortega <alfredortegaiii@gmail.com>
2019-01-21 08:25:59 -0600

    completed project

--- todo.dev/resources/views/todos/show.blade.php
+++ todo.dev/resources/views/todos/show.blade.php
@@ -7,4 +7,12 @@
     <div class="label label-danger">{{ $todo->due }}</div>
     <hr>
     <p class="" >{{$todo->body}}</p>
-    @endsection
+    <br/>
+    <br/>
+    <a href="/todo/{{$todo->id}}/edit" class="btn btn-default">Edit</a>
+    {!! Form::open(['action' => ['TodosController@destroy', $todo->id], 'method' => 'POST', 'class' => 'pull-right' ]) !!}
+        {{ Form::hidden('_method','DELETE') }}
+        {{ Form::bsSubmit('Delete', ['class' => 'btn btn-danger']) }}
+    {!! Form::close() !!}
+
+@endsection
